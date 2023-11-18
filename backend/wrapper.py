dc45f8496198ad60359e743c705a34377ff2bb71
Carolin <carolin.ganahl@tum.de>
2023-11-18 11:59:29 +0100

    substring fpor cs file

--- backend/wrapper.py
+++ backend/wrapper.py
@@ -2,6 +2,7 @@ import os
 import openai 
 from config import OPENAI_API_KEY
 import json
+import re
 
 openai.api_key = OPENAI_API_KEY
 content = None
@@ -16,7 +17,13 @@ completion = openai.chat.completions.create(
     {"role": "user", "content": "Replace in this receipe list the ingredient Pfeffer with Jasminreis where it appears and return just the new js file:" + str(content)}
   ]
 )
-
+response = str(completion.choices[0].message.content)
+cs_file = None
+index=response.find("[")
+if index !=-1:
+    cs_file = response[index:] 
+else:
+    raise Exception('Sub string not found!')
 f = open("backend/output.txt", "a")
-f.write(str(completion.choices[0].message.content))
+f.write(cs_file)
 f.close()
