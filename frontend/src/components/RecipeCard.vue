00272a15e22213eb68ac89db4732223eb63a488e
Florian Schmidt <fs.schmidt@tum.de>
2023-11-18 12:03:10 +0100

    add badges

--- frontend/src/components/RecipeCard.vue
+++ frontend/src/components/RecipeCard.vue
@@ -4,7 +4,17 @@
         <div class="card-body">
             <h5 class="card-title">{{ recipe.name }}</h5>
             <p class="card-text">{{ recipe.headline }}</p>
-            <button @click="dismissRecipe" class="btn btn-danger">Dismiss</button>
+            <div class="d-flex justify-content-between">
+                <div>
+                    <span class="badge bg-primary" :key="tag.name" v-for="tag in recipe.tags">
+                        {{ tag.name }}
+                    </span>
+                </div>
+                <button @click="dismissCard" class="btn btn-danger">Dismiss</button>
+            </div>
+
+            <hr>
+            <p class="card-text"><strong>Recommendation:</strong> Based on your preferences.</p>
         </div>
     </div>
 </template>
