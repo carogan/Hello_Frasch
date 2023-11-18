31a6ae2121b1dec38ffa42278f99f36a52a8c7a3
Carolin <carolin.ganahl@tum.de>
2023-11-18 15:49:40 +0100

    promt works

--- backend/wrapper.py
+++ backend/wrapper.py
@@ -10,13 +10,16 @@ with open('backend/mockedRecipes.json','r') as f:
    content = json.load(f)
 
 #content = json.load("backend/mockedRecipes.js")
+ingredient = "pork"
 completion = openai.chat.completions.create(
   model="gpt-4",
   messages=[
     {"role": "system", "content": "Food lover"},
-    {"role": "user", "content": "The user does not like pork, Replace in this receipe all ingredients with pork where they appears with the right amount of a similar ingedrient which is used in another dish of the list (with the exact same title) and return just the new json object thus the response should start with [{\"index\": 1,... and end with}]:" + json.dumps(content)}
+    {"role": "user", "content": "The user does not like " + ingredient +", Replace in this receipe all ingredients with " + ingredient +" where they appears with the right amount of a similar ingedrient in taste and usage which is used in another dish of the list (with the exact same name and type). If absolutly no possible replacemant exists, do not replace it and instead change the name of the ingredient to the original name +  no replacement possible. Only do this in the list of ingredients. Do not invent a new ingredient name that was not used in a different ingredient. Return just the new json object thus the response should start with [{\"index\": 1,... and end with}]:" + json.dumps(content)}
   ]
 )
+#with open("backend/output.json", 'w') as json_file:
+#    json.dump(str(completion.choices[0].message.content), json_file, indent=1)
 response = str(completion.choices[0].message.content)
 f = open("backend/output.json", "a")
 f.write(response)
