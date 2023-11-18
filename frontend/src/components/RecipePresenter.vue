1b1aa71c22f9e6308dd0161c06633f1dbc0bf706
Florian Schmidt <fs.schmidt@tum.de>
2023-11-18 18:03:58 +0100

    add working state keeping for collapsing cards

--- frontend/src/components/RecipePresenter.vue
+++ frontend/src/components/RecipePresenter.vue
@@ -1,23 +1,29 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col" v-for="(recipe, index) in recipes.slice(0, 3)" :key="index">
+            <div class="col" v-for="(recipe, index) in recipes" :key="index">
                 <dismissed-recipe-card v-if="recipe.dismissed" :recipe="recipe"></dismissed-recipe-card>
                 <recipe-card :recipe="recipe" v-if="!recipe.dismissed" @replace="replaceRecipe">
                 </recipe-card>
             </div>
         </div>
 
-        <!--
-        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#newRecipeCollapse" aria-expanded="false" aria-controls="newRecipeCollapse">
-            Toggle Collapse
-        </button>
-         -->
-
         <div class="row">
             <div class="collapse" id="newRecipeCollapse">
                 <div class="card card-body">
-                    Collapsible content goes here.
+                    <h2>Choose your new recipe</h2>
+
+                    <button
+                        class="btn btn-primary"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#newRecipeCollapse"
+                        aria-expanded="false"
+                        aria-controls="newRecipeCollapse"
+                        @click="enlargeAndReplace"
+                    >
+                        Toggle Collapse
+                    </button>
                 </div>
             </div>
         </div>
@@ -36,13 +42,27 @@ export default {
     },
     data() {
         return {
-            recipes: mockedRecipes.map(recipe => ({...recipe, dismissed: false}))
+            recipes: mockedRecipes.map(recipe => ({...recipe, dismissed: false})).slice(0, 3),
+            previousRecipes: mockedRecipes.map(recipe => ({...recipe, dismissed: false})).slice(0, 3),
+            replacementRecipeChoices: mockedRecipes.slice(3, 6),
+            replacedRecipeIndex: null
         };
     },
     methods: {
-        replaceRecipe(replacedRecipce) {
-            replacedRecipce.dismissed = true;
+        replaceRecipe(replacedRecipe) {
+            // ugly way to deep copy an array
+            this.previousRecipes = JSON.parse(JSON.stringify(this.recipes));
+
+            // can use index here
+            this.replacedRecipeIndex = replacedRecipe.index - 1;
+            this.previousRecipes[this.replacedRecipeIndex].dismissed = true;
+
+            // after we took note of which we minimized, make all of them smaller
+            this.recipes.forEach(r => r.dismissed = true);
         },
+        enlargeAndReplace() {
+            this.recipes = this.previousRecipes;
+        }
     },
 }
 </script>
