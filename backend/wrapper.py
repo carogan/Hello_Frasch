5da8ad418e9fc49e4e5c3ae831b60accb30d29e5
Carolin <carolin.ganahl@tum.de>
2023-11-19 01:03:56 +0100

    new recipes with nutrions completed

--- backend/wrapper.py
+++ backend/wrapper.py
@@ -14,7 +14,7 @@ with open('backend/mockedRecipes_with_Nutrition.json','r') as f:
 
 #content = json.load("backend/mockedRecipes.js")
 ingredient = "Lime"
-content = recipes[2]
+content = recipes[1]
 correct_json = False
 while correct_json is False:
 
@@ -22,7 +22,7 @@ while correct_json is False:
     model="gpt-4",
     messages=[
         {"role": "system", "content": "Food lover"},
-        {"role": "user", "content": "I want to make a recipe, but it is low in Iron, please replace one ingredient so that the iron amount increases. Return just the new json object and the json object has to have correct syntax, thus the response should start with [{\"index\": 1,... and end with}] :" + json.dumps(content)}    #with open("backend/output.json", 'w') as json_file:
+        {"role": "user", "content": "I want to make a recipe, but it is low in Vitamine E, please replace some ingredients so that the vitamine e amount increases. Return the whole new receipe as json object. Just return the json object and the json object has to have correct syntax, thus the response should start with [{\"index\": 1,... and end with}] :" + json.dumps(content)}    #with open("backend/output.json", 'w') as json_file:
         ])
     #    json.dump(str(completion.choices[0].message.content), json_file, indent=1)
     response = str(completion.choices[0].message.content)
